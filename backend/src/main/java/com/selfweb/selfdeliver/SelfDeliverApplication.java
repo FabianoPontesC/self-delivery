35ca3d62121bb24d36ce235fe5552c4eab61330f
Fabiano Pontes <fabiano.pontesc@gmail.com>
2021-01-04 17:40:53 -0300

    Project created

---
+++ backend/src/main/java/com/selfweb/selfdeliver/SelfDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.selfweb.selfdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfDeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/selfweb/selfdeliver/SelfDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfweb.selfdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
